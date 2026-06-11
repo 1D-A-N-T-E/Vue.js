527bf7f81487650be5fb314fae2b3054d8bd32a6
Marcis Graudins <mgraudins60@gmail.com>
2026-06-11 22:04:08 +0300

    Mans vue.js projekts

---
+++ vue-project/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+ <footer class="fixed bottom-0 left-0 w-full h-40">
+ <div class="grid grid-cols-2 justify-items-center h-lvh   ">
+    <div>
+        <p>CONTACT INFORMATION</p>
+        <p>Phone Number: +371 5454554</p>
+        <p>Email: mgraudins@gmail.com</p>
+    </div>
+    <div class="justify-center  ">
+        <p>SOCIAL MEDIA</p>
+        <P>Instagram:mgraudins</P>
+        <p>GitHub</p>
+    </div>
+</div>
+</footer>
+</template>
